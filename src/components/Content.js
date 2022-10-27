0d37c214914158cf612a88c1bd2106a2a6ece87e
Oyedeji-Opeoluwa <noblefarz7@mail.com>
2022-10-27 23:44:23 +0100

    last commits

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,6 +3,42 @@ import './Content.css';
 function Content() {
   const style = { color: "black",  }
 
+  const handleHover=()=>{
+    document.querySelector('#mobileHover').style.display="block"
+    document.querySelector('#mobile').style.display="none"
+  }
+  const handleOut=()=>{
+    document.querySelector('#mobileHover').style.display="none"
+    document.querySelector('#mobile').style.display="block"
+  }
+  const setClick=()=>{
+    document.querySelector('#mobileHover').style.display="none"
+    document.querySelector('#mobile').style.display="none"
+    document.querySelector('.icon4').style.display="block"
+  }
+  const performClick=()=>{
+    document.querySelector('#mobileHover').style.display="block"
+    document.querySelector('#mobile').style.display="none"
+    document.querySelector('.icon4').style.display="none"
+  }
+  const workEnter=()=>{
+    document.querySelector('#default').style.display="none"
+    document.querySelector('.icon1').style.display="block"
+  }
+  const handleave=()=>{
+    document.querySelector('#default').style.display="block"
+    document.querySelector('.icon1').style.display="none"
+  }
+  const handleClick=()=>{
+    document.querySelector('.icon11').style.display="block"
+    document.querySelector('.icon1').style.display="none"
+  }
+  const actionClick=()=>{
+    document.querySelector('.icon11').style.display="none"
+    document.querySelector('.icon1').style.display="block"
+  }
+
+
   return (
    
     <div className='contents'>
@@ -17,8 +53,13 @@ function Content() {
             </div>
 
             <div className='name'>Annette Black</div>
-            <div className='icon'><img src='./images/arrow.svg'></img></div>
-            <div className='icon2'><img src='./images/menu.png'></img></div>
+            <div className='icon'><img src='./images/arrow.svg' id='default' onMouseEnter={workEnter}></img></div>
+            <div className='icon1'><img src='./images/sharestate.png' onMouseOut={handleave} onClick={handleClick}></img></div>
+            <div className='icon11'><img src='./images/focus.png' onClick={actionClick}></img></div>
+            <div className='icon3'><img src='./images/mobilesharehover.png' id='mobileHover' onMouseOut={handleOut} onClick={setClick}></img></div>
+            <div className='icon4'><img src='./images/mobilefocus.png' onClick={performClick}></img></div>
+            <div className='icon2'><img src='./images/menu.png'id='mobile' onMouseOver={handleHover}></img></div>
+
         
           
         </div>
