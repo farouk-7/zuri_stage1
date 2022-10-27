da77ac989a18bb6e764f160bd1b3218448e53ca8
Oyedeji-Opeoluwa <noblefarz7@mail.com>
2022-10-27 19:42:34 +0100

    final commit

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,8 +5,8 @@ function Social() {
   return (
     <div className='contents-container'>
       <div className='social-icons'>
-          <div id='slack'><img src='./images/slack.png'></img>
-          <h1>KingFarouk</h1></div>
+          <div id='slack-img'><img src='./images/slack.png'></img>
+          <h1 id='slack'>KingFarouk</h1></div>
           <a href='https://github.com/farouk-7'
           className='git'><i class="fa-brands fa-github"></i></a>
           
